c9cd4d2f9c4450918563ae83981ba8d5423e8d11
viniciu scarabelli <vinicius.scarabelli@cittamobi.com.br>
2021-09-18 23:31:54 -0300

    :tada: calculator done

--- calculator.js
+++ calculator.js
@@ -1,11 +1,51 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const port = 8080;
+
 const app = express();
-port = 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/styles'))
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(`${__dirname}/index.html`)
 })
-app.post()
+app.get("/calculator", (req, res) => {
+  res.sendFile(`${__dirname}/calculator.html`)
+})
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(`${__dirname}/bmiCalculator.html`)
+})
+
+app.post("/calculator", (req, res)=>{
+  let num1 = Number(req.body.n1);
+  let num2 = Number(req.body.n2);
+  let op = req.body.operation;
+  let result;
+  switch (op){
+    case "plus" :
+      result = num1 + num2;
+      break;
+    case "sub":
+      result = num1 - num2;
+      break;
+    case "mult":
+      result = num1 * num2;
+      break;
+    case "div":
+      result = num1 / num2;
+      break;
+    default:
+      res.error("Not a operation");
+  }
+  res.send(`The result of the calculation is ${result}`);
+})
+app.post("/bmicalculator", (req, res)=>{
+  let num1 = parseFloat(req.body.weight);
+  let num2 = parseFloat(req.body.height);
+  let result = num1 / (num2 * num2);
+  res.send(`Your BMI is ${result}`);
+})
+
 app.listen(port, ()=>{
  console.log(`Server started on port: ${port}`)
 });
--- readme.md
+++ readme.md
@@ -2,3 +2,22 @@
 a simple calculator project to learn simple things in expressJS like how to do post and get request.
 
 everything has a start point, right?
+
+## Do you wish to use it?
+
+Just do the usual
+``` bash
+git clone  https://github.com/
+```
+then run
+
+``` bash
+# install node packages with
+ npm install
+
+# after that just run the dev server
+ npm start
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
